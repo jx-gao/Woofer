844c60c251efd1d7788042999a587d2a56da4412
JX Gao <jiaxinabecgao@gmail.com>
2021-06-11 23:23:18 +0200

    add friend using username complete

--- app/src/main/java/com/example/woofer/AddFriend.java
+++ app/src/main/java/com/example/woofer/AddFriend.java
@@ -2,13 +2,17 @@ package com.example.woofer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddFriend extends AppCompatActivity {
 
-    String username;
+    private String username;
+    private EditText editTextFriendUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,28 @@ public class AddFriend extends AppCompatActivity {
         setContentView(R.layout.activity_add_friend);
         Intent intent = getIntent();
         username = intent.getStringExtra("username");
-        Toast.makeText(AddFriend.this, username,Toast.LENGTH_LONG).show();
+    }
+
+    public void addFriend(String friendUsername){
+        ContentValues cv = new ContentValues();
+        cv.put("username", username);
+        cv.put("friend", friendUsername);
+
+        PHPRequest request = new PHPRequest();
+        request.doRequest(AddFriend.this, "addfriend", cv, new RequestHandler() {
+            @Override
+            public void proccessResponse(String response) {
+                Toast.makeText(AddFriend.this, response, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void doAddFriend(View view) {
+        editTextFriendUsername = (EditText) findViewById(R.id.editTextFriendUsername);
+        if(editTextFriendUsername.getText().toString().equals("")){
+            Toast.makeText(AddFriend.this, "Enter friend username", Toast.LENGTH_LONG).show();
+            return;
+        }
+        addFriend(editTextFriendUsername.getText().toString());
     }
 }
