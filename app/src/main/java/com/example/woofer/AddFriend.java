3bdb186344970b32269d819de40fc515f2ee31c6
JX Gao <jiaxinabecgao@gmail.com>
2021-06-12 15:09:29 +0200

    add friends complete

--- app/src/main/java/com/example/woofer/AddFriend.java
+++ app/src/main/java/com/example/woofer/AddFriend.java
@@ -4,22 +4,97 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class AddFriend extends AppCompatActivity {
 
     private String username;
     private EditText editTextFriendUsername;
+    LinearLayout fofContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
         Intent intent = getIntent();
+
         username = intent.getStringExtra("username");
+        fofContainer = (LinearLayout) findViewById(R.id.fofLinearView);
+
+        doGetFOF();
+    }
+
+    private void createLabels() {
+        TextView friend = new TextView(this), fof = new TextView(this);
+        friend.setText("Friend:");
+        friend.setTextSize(15);
+        friend.setPadding(20,20,50,20);
+        fof.setText("Fof:");
+        fof.setTextSize(15);
+        fof.setPadding(20,20,20,20);
+
+        LinearLayout labelLayout = new LinearLayout(this);
+        labelLayout.setOrientation(LinearLayout.HORIZONTAL);
+        labelLayout.addView(friend);
+        labelLayout.addView(fof);
+
+        labelLayout.setBackgroundColor(Color.parseColor("#ca9b52"));
+        fofContainer.addView(labelLayout);
+    }
+
+    private void doGetFOF() {
+        createLabels();
+        ContentValues cv = new ContentValues();
+        cv.put("username", username);
+        PHPRequest request = new PHPRequest();
+        request.doRequest(AddFriend.this, "fof", cv, new RequestHandler() {
+            @Override
+            public void proccessResponse(String response) {
+                processJSON(response);
+            }
+        });
+    }
+
+    public void processJSON(String json){
+        try {
+            JSONArray ja = new JSONArray(json);
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject jo = ja.getJSONObject(i);
+                FOFLayout fofL = new FOFLayout(this);
+                fofL.populate(jo);
+                if(i%2==0){
+                    fofL.setBackgroundColor(Color.parseColor("#EEEEFF"));
+                }
+                fofContainer.addView(fofL);
+
+                fofL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            addFriend(jo.getString("USERNAME2"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void addFriend(String friendUsername){
@@ -32,6 +107,8 @@ public class AddFriend extends AppCompatActivity {
             @Override
             public void proccessResponse(String response) {
                 Toast.makeText(AddFriend.this, response, Toast.LENGTH_LONG).show();
+                fofContainer.removeAllViews();
+                doGetFOF();
             }
         });
     }
@@ -43,5 +120,10 @@ public class AddFriend extends AppCompatActivity {
             return;
         }
         addFriend(editTextFriendUsername.getText().toString());
+    }
+
+    public void doRefreshFOF(View view) {
+        fofContainer.removeAllViews();
+        doGetFOF();
     }
 }
---
+++ app/src/main/java/com/example/woofer/FOFLayout.java
@@ -0,0 +1,39 @@
+package com.example.woofer;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class FOFLayout extends LinearLayout {
+
+    TextView friend, fof;
+
+    public FOFLayout(Context context) {
+        super(context);
+        setOrientation(LinearLayout.HORIZONTAL);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.weight = 0;
+
+        friend = new TextView(context);
+        friend.setPadding(20,20,50,20);
+        friend.setTextSize(15);
+        addView(friend);
+
+
+        fof = new TextView(context);
+        fof.setPadding(20,20,20,20);
+        fof.setTextSize(15);
+
+        addView(fof);
+    }
+
+    public void populate(JSONObject jo) throws JSONException {
+        friend.setText(jo.getString("USERNAME1"));
+        fof.setText(jo.getString("USERNAME2"));
+    }
+}
