bd6e0ece81f925fb4133a2df1d2779c94ce562cc
ULTRAWHIZKID <65918215+ULTRAWHIZKID@users.noreply.github.com>
2021-06-13 16:28:17 +0200

    Status Functionality and UI
    UI update for Status screen

--- app/src/main/java/com/example/woofer/Login.java
+++ app/src/main/java/com/example/woofer/Login.java
@@ -36,7 +36,7 @@ public class Login extends AppCompatActivity {
                 JSONObject jo = ja.getJSONObject(0);
                 if(jo.getString("Password").equals(editTextPassword.getText().toString())){
                     Intent intent = new Intent(getApplicationContext(), Status.class);
-                    intent.putExtra("username", jo.getString("USERNAME"));
+                    intent.putExtra("username", jo.getString("Username"));
 
                     startActivity(intent);
                     finish();
